af2e0203f89829f03c9937715d104e01a20ace12
TrueStrikes <137821671+TrueStrikes@users.noreply.github.com>
2023-07-24 14:40:24 -0400

    Update command.py

--- command.py
+++ command.py
@@ -28,18 +28,26 @@ def main():
     print_message("Clipboard monitor started..", Fore.RED)
     prefix = read_prefix_from_file("prefix.txt")
 
+    # Initialize the previous clipboard text
+    prev_clipboard_text = ""
+
     while True:
         # Get the URL from the clipboard
         clipboard_text = pyperclip.paste()
 
-        # Extract the ID from the URL
-        item_id = extract_id_from_url(clipboard_text)
+        # Check if clipboard text has changed
+        if clipboard_text != prev_clipboard_text:
+            # Update the previous clipboard text
+            prev_clipboard_text = clipboard_text
 
-        if item_id:
-            # Set the clipboard with the formatted text
-            clipboard_formatted = f"{prefix}aid {item_id}" if prefix else f"aid {item_id}"
-            pyperclip.copy(clipboard_formatted)
-            print_message(f"Extracted ID: {item_id} | Copied to clipboard: {clipboard_formatted}", Fore.GREEN)
+            # Extract the ID from the URL
+            item_id = extract_id_from_url(clipboard_text)
+
+            if item_id:
+                # Set the clipboard with the formatted text
+                clipboard_formatted = f"{prefix}aid {item_id}" if prefix else f"aid {item_id}"
+                pyperclip.copy(clipboard_formatted)
+                print_message(f"Extracted ID: {item_id} | Copied to clipboard: {clipboard_formatted}", Fore.GREEN)
 
 if __name__ == "__main__":
     # Clear the console before starting the loop
